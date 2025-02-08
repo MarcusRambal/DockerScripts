37b4c910444fedf7eae8a95c9ba32b03c58776d9
Marcus Rambal Preston <128008102+MarcusRambal@users.noreply.github.com>
2025-02-07 19:43:15 -0500

    Update main.c

--- c/main.c
+++ c/main.c
@@ -1,6 +1,42 @@
-#include <stdio.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <time.h>
 
-int main() {
-    printf("Hola, mundo!\n");
+void generate_matrix(int n, int matrix[n][n]) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            matrix[i][j] = rand() % 10;
+        }
+    }
+}
+
+void multiply_matrices(int n, int A[n][n], int B[n][n], int C[n][n]) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            C[i][j] = 0;
+            for (int k = 0; k < n; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
+
+void benchmark(int n) {
+    int A[n][n], B[n][n], C[n][n];
+    srand(time(NULL));
+    generate_matrix(n, A);
+    generate_matrix(n, B);
+
+    clock_t start = clock();
+    multiply_matrices(n, A, B, C);
+    clock_t end = clock();
+
+    double elapsed_ms = (double)(end - start) / CLOCKS_PER_SEC * 1000;
+    printf("C: %.3f ms\n", elapsed_ms);
+}
+
+int main(int argc, char *argv[]) {
+    int n = (argc > 1) ? atoi(argv[1]) : 300;
+    benchmark(n);
     return 0;
 }
